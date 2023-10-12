d908f1b2fc88369bb778b5687212e6b12b57afad
renas <Emails      Renask.dev@gmail.com>
2023-10-13 01:40:57 +0200

    Structure/Routing/Header

---
+++ views/home.views.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welcome to ProfielPlus!</title>
+    <link rel="stylesheet" href="../views/css/default.css">
+    <link rel="stylesheet" href="../views/css/home.css">
+</head>
+<body class="container">
+<header class="header"><?php @require 'partials/navbar.php'?></header>
+<div class="main">
+    <h1>Hello</h1>
+    <h2>purplemeow</h2>
+</div>
+
+<footer class="footer">footer!</footer>
+</body>
+</html>
+<?php
+?>
---
+++ views/partials/navbar.php
@@ -0,0 +1,13 @@
+<link rel="stylesheet" href="../../views/css/navbar.css">
+<div class="nav">
+    <img class="logo" src="../../views/public/images/angrycatlogo.png" alt="Moewie">
+<nav class="nav">
+
+    <ul class="nav-items">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About us</a></li>
+        <li><a href="#">Register</a></li>
+    </ul>
+</nav>
+<a href="#"><button class="Login-button">Login</button></a>
+</div>
