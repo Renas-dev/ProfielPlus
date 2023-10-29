e603694d573aadbca0d854f804fbec376f29690d
Sadek <sharkalmousawi@live.com>
2023-10-29 17:56:21 +0100

    Admin update user password fixes.

--- functions/adminUserUpdate.php
+++ functions/adminUserUpdate.php
@@ -3,24 +3,31 @@ require_once '../includes/dbh.inc.php';
 
 $id = $_POST['userid'];
 
-
 if (isset($_POST['submit'])) {
-    $firstname = ($_POST['firstname']);
-    $lastname = ($_POST['lastname']);
-    $studentnumber = ($_POST['student_number']);
-    $telephone = ($_POST['telephone']);
-    $username = ($_POST['username']);
-    $email = ($_POST['email']);
-    $pwd = ($_POST['pwd']);
-    $sql = "update users set firstname='$firstname', lastname='$lastname', student_number='$studentnumber', telephone='$telephone'
-             , username='$username' , email='$email', pwd=:pwd where id=$id";
-    $query = $pdo->prepare($sql);
-    $query->bindParam(":pwd", $hashedPwd);
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $studentnumber = $_POST['student_number'];
+    $telephone = $_POST['telephone'];
+    $username = $_POST['username'];
+    $email = $_POST['email'];
 
-    $options = [
-        'cost' => 12
-    ];
-    $hashedPwd = password_hash($pwd, PASSWORD_BCRYPT, $options);
+    // Check if PWD field is not empty
+    if (!empty($_POST['pwd'])) {
+        $pwd = $_POST['pwd'];
+        $options = [
+            'cost' => 12
+        ];
+        $hashedPwd = password_hash($pwd, PASSWORD_BCRYPT, $options);
+        $sql = "UPDATE users SET firstname='$firstname', lastname='$lastname', student_number='$studentnumber', telephone='$telephone', username='$username', email='$email', pwd=:pwd WHERE id=$id";
+    } else {
+        // If PWD is empty dont change PWD field
+        $sql = "UPDATE users SET firstname='$firstname', lastname='$lastname', student_number='$studentnumber', telephone='$telephone', username='$username', email='$email' WHERE id=$id";
+    }
+
+    $query = $pdo->prepare($sql);
+    if (!empty($_POST['pwd'])) {
+        $query->bindParam(":pwd", $hashedPwd);
+    }
 
     $query->execute();
     header('location: /admin');
